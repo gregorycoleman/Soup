a6ba7fb6f73f6643fb1bc8c45829360ee3f54452
Develop <develop@M1-MacBook-Pro.local>
2021-06-23 10:35:37 +0800

    Updated docs

--- src/Souporm.php
+++ src/Souporm.php
@@ -1,9 +1,28 @@
 <?php
 
-/*
+/** 
 * namespace Gregorycoleman\Souporm;
 * 
 * $t = new Souporm() ;
+*
+*
+* each record has a pointer to previous record
+* each record has a computed hash based on previous has
+* each record has a blockchain algo version number
+* each record has a uuid
+* each record has a datafield
+* 
+* The object is a special type of record that has 
+* a uuid , but no value
+* Data Key types
+*
+* +---------------------------------+
+* | type |
+* +---------------------------------+
+* | Object    | object
+* | User Data | user
+* +---------------------------------+
+* 
 */
 
 namespace Gregorycoleman\Souporm;
@@ -11,6 +30,43 @@ namespace Gregorycoleman\Souporm;
 class Souporm
 {
     public static function hi(){
-        echo "Laravel Soup Package";
+        echo "Laravel Soup Package" ;
+    }
+
+    /**
+     * Creates a record. Returns the uuid, but does not save 
+     * the object. Sets things like creation time, etc.
+     */
+    public function create() {
+
+    }
+
+    /**
+     * Write all object records to db
+     */
+    public function save() {
+
+    }
+
+    /**
+     * Debugging function
+     */
+    public function print() {
+
+    }
+
+    /**
+     * Sets a key value in the opject, this is a user 
+     * defined variable
+     */
+    public function set($key, $value) {
+        $this->set_type($key, $value, "user") ;
+    }
+
+    /**
+     * Sets a key value in the opject
+     */
+    private function set_type($key, $value, $type) {
+
     }
 }
